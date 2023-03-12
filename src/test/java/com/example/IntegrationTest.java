289f8f2a73c88609b3f6d28ad4ca1d4d5747a419
lgr21 <francislgr@outlook.com>
2023-03-12 18:02:27 +0100

    Integration test cases added

--- src/test/java/com/example/IntegrationTest.java
+++ src/test/java/com/example/IntegrationTest.java
@@ -5,8 +5,32 @@ import org.junit.Test;
 
 public class IntegrationTest {
     Calculator testObj=new Calculator();
+
     @Test
-    public void calculatorTest()
+    public void calculatorAddTest()
+    {
+        assertEquals(testObj.calculator("1+2+3+4+(4+2)+0"),"16");
+    }
+
+    @Test
+    public void calculatorSubTest()
+    {
+        assertEquals(testObj.calculator("1-2-3-4-(4-2)-0"),"-10");
+    }
+
+    @Test
+    public void calculatorMulTest()
+    {
+        assertEquals(testObj.calculator("1*2*3*4*5*(1*1)"),"120");
+    }
+
+    @Test
+    public void calculatorDivTest()
+    {
+        assertEquals(testObj.calculator("1/2/3/4/5/6"),"0.001388888888888889");
+    }
+    @Test
+    public void calculatorElementaryArithmeticTest()
     {
         assertEquals(testObj.calculator("(1+2)*3+6/2"),"12");
         assertEquals(testObj.calculator("2/3+3*2-10"),"-3.333333333333333");
